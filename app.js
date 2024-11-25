76a52b8b9f93d8511a9f5037a6fc798eda28e020
angelasandom <angelasandom@gmail.com>
2024-11-25 13:51:20 +0000

    completed task 2 get api

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { getApi } = require("./controllers/api.controllers");
+const app = express();
+
+app.get("/api", getApi);
+
+
+module.exports = app;
--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,13 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
+console.log(`Environment: ${ENV}`);
+console.log(`PGDATABASE: ${process.env.PGDATABASE}`);
+
+
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
 }
 
 module.exports = new Pool();
+
