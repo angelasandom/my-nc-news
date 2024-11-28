b0ecec22e3b7f0db36d4e21908e1351216d44f31
angelasandom <angelasandom@gmail.com>
2024-11-28 11:13:14 +0000

    working on task 14 hosting

--- db/connection.js
+++ db/connection.js
@@ -8,10 +8,17 @@ require('dotenv').config({
 console.log(`Environment: ${ENV}`);
 console.log(`PGDATABASE: ${process.env.PGDATABASE}`);
 
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL; 
+  config.max = 2;
+}
+
+
+module.exports = new Pool(config);
 
